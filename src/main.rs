7c156f4aa43b32f6f00b09e65b7d207083c68936
James Madden <jamesbmadden@gmail.com>
2021-04-28 19:56:48 -0700

    add web compatibility

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,29 @@ use winit::{
   window::WindowBuilder,
 };
 
-const WIDTH = 10;
-const HEIGHT = 10;
+const WIDTH = 11;
+const HEIGHT = 11;
 
 fn main() {
   // generate the world
-  let world = worldgen::elevation_to_tiles(worldgen::generate_perlin(11, 11));
+  let world = worldgen::elevation_to_tiles(worldgen::generate_perlin(WIDTH, HEIGHT));
   // create a window
   let event_loop = EventLoop::new();
   let window = WindowBuilder::new().build(&event_loop).unwrap();
+  // create a canvas if we're running in web
+  #[cfg(feature = "web-sys")]
+  {
+    use winit::platform::web::WindowExtWebSys;
+
+    let canvas = window.canvas();
+
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let body = document.body().unwrap();
+
+    body.append_child(&canvas)
+      .expect("Append canvas to HTML body");
+  }
   // send to renderer
-  let renderer = render::Render::new();
+  let renderer = render::Render::new(&window);
 }
