6d3e99401812e2b884bcac6556ae0135f7ab5519
James Madden <jamesbmadden@gmail.com>
2021-04-30 16:18:32 -0700

    Implement basic momentum movement (wip)

--- src/camera.rs
+++ src/camera.rs
@@ -7,6 +7,8 @@ pub struct Camera {
   pub height: i32,
   pub x: f32,
   pub y: f32,
+  pub x_speed: f32,
+  pub y_speed: f32,
   pub uniforms: Uniforms
 }
 
@@ -15,7 +17,7 @@ impl Camera {
   pub fn new (width: i32, height: i32) -> Self {
     Camera {
       keys_down: HashSet::new(),
-      x: 0., y: 0.,
+      x: 0., y: 0., x_speed: 0., y_speed: 0.,
       width, height,
       uniforms: Uniforms::default()
     }
@@ -23,20 +25,25 @@ impl Camera {
 
   // if keys are pressed, update x and y values
   pub fn update (&mut self) {
-    let speed = 0.5;
+    // movement speed stuff
+    self.x_speed /= 1.2;
+    self.y_speed /= 1.2;
 
     if self.keys_down.contains(&winit::event::VirtualKeyCode::A) {
-      self.x -= speed;
+      self.x_speed -= 0.2;
     }
     if self.keys_down.contains(&winit::event::VirtualKeyCode::D) {
-      self.x += speed;
+      self.x_speed += 0.2;
     }
     if self.keys_down.contains(&winit::event::VirtualKeyCode::S) {
-      self.y += speed;
+      self.y_speed += 0.2;
     }
     if self.keys_down.contains(&winit::event::VirtualKeyCode::W) {
-      self.y -= speed;
+      self.y_speed -= 0.2;
     }
+
+    self.x += self.x_speed;
+    self.y += self.y_speed;
 
     // prevent from raising or lowering the x or y past bounds
     if self.x < 0. {
@@ -49,8 +56,9 @@ impl Camera {
     // make transforms for how much to offset tiles for smoother scrolling
     let x_offset = self.x as f32 % 1.;
     let y_offset = self.y as f32 % 1.;
-    let tile_width = 1. / self.width as f32;
-    let tile_height = 1. / self.height as f32;
+    // coordinate space is -1.0 to 1.0, so size should be double as big as 1 / size
+    let tile_width = 2. / self.width as f32;
+    let tile_height = 2. / self.height as f32;
     // set as uniforms to be rendered with
     self.uniforms = Uniforms { translate_vector: [ x_offset * tile_width, y_offset * tile_height ] };
 
